0b1b81476a9767ddde42bb42af610f15abb872a1
Sune Westphalen <sune.westphalen@velocityapp.com>
2021-08-20 10:55:36 +0100

    feat: initial version

---
+++ README.md
@@ -0,0 +1,10 @@
+# ULID to HEX
+
+May be useful if you are storing ULIDs in binary format, and your database CLI already supports querying by HEX values.
+
+## Usage
+
+```bash
+npx ulid-to-hex 01FCB79DXEHPQJRD8BWC6G7PVH
+> 0x017B1674B7AE8DAF2C350BE30D03DB71
+```
---
+++ index.js
@@ -0,0 +1,12 @@
+const { Ulid } = require('id128')
+
+const value = process.argv[2]
+
+if (!value) {
+  console.error('Please pass in a ULID')
+  process.exit(1)
+}
+
+const hex = Buffer.from(Ulid.fromCanonical(value).bytes).toString('hex')
+
+console.log(`0x${hex.toUpperCase()}`)
