63ec582346fd98d2bdad26fc7e21c5ec49a41614
Sune Westphalen <sune.westphalen@velocityapp.com>
2021-10-15 11:32:05 +0100

    feat: handle ulid, uuid and hex all in one

--- README.md
+++ README.md
@@ -4,7 +4,44 @@ May be useful if you are storing ULIDs in binary format, and your database CLI a
 
 ## Usage
 
+**Parse ULID**
+
 ```bash
 npx ulid-to-hex 01FCB79DXEHPQJRD8BWC6G7PVH
 > 0x017B1674B7AE8DAF2C350BE30D03DB71
+> 017b1674-b7ae-8daf-2c35-0be30d03db71
+```
+
+**Parse HEX**
+
+```bash
+npx ulid-to-hex 0x017B1674B7AE8DAF2C350BE30D03DB71
+> 01FCB79DXEHPQJRD8BWC6G7PVH
+> 017b1674-b7ae-8daf-2c35-0be30d03db71
+```
+
+**Parse UUID**
+
+```bash
+npx ulid-to-hex 017b1674-b7ae-8daf-2c35-0be30d03db71
+> 0x017B1674B7AE8DAF2C350BE30D03DB71
+> 01FCB79DXEHPQJRD8BWC6G7PVH
+```
+
+### Flags
+
+If you want just one value to be returned, you can use the flags `--uuid` or `--ulid`.
+
+Example: **Parse ULID to UUID** only
+
+```bash
+npx ulid-to-hex 01FCB79DXEHPQJRD8BWC6G7PVH --uuid
+> 017b1674-b7ae-8daf-2c35-0be30d03db71
+```
+
+Example: **Parse HEX to ULID** only
+
+```bash
+npx ulid-to-hex 0x017B1674B7AE8DAF2C350BE30D03DB71 --ulid
+> 01FCB79DXEHPQJRD8BWC6G7PVH
 ```
--- index.js
+++ index.js
@@ -1,13 +1,89 @@
 #!/usr/bin/env node
-const { Ulid } = require('id128')
+const {Ulid, Uuid} = require('id128')
 
-const value = process.argv[2]
-
-if (!value) {
-  console.error('Please pass in a ULID')
-  process.exit(1)
+const error = (message, code = 1) => {
+  console.error(message)
+  process.exit(code)
 }
 
-const hex = Ulid.fromCanonical(value).toRaw()
+const {value, mode} = process.argv.slice(2).reduce((acc, arg) => {
+  if (arg.startsWith('-')) {
+    return {
+      ...acc,
+      mode: (() => {
+        switch (arg.replace(/^--?/, '')) {
+          case 'ulid':
+            return 'ulid'
 
-console.log(`0x${hex}`)
+          case 'uuid':
+            return 'uuid'
+
+          default:
+            error(`Unknown argument ${arg}`)
+        }
+      })(),
+    }
+  }
+
+  if (acc.value) {
+    error('Too many arguments')
+  }
+
+  return {
+    ...acc,
+    value: arg.replace(/^0x/, ''),
+  }
+}, {})
+
+if (!value) {
+  error('Please pass in a ULID')
+}
+
+(() => {
+  // ULID to HEX/UUID
+  if (Ulid.isCanonical(value)) {
+    const hex = Ulid.fromCanonical(value).toRaw()
+
+    if (mode !== 'uuid') {
+      console.log(`0x${hex}`)
+    }
+
+    if (mode !== 'ulid') {
+      console.log(Uuid.fromRaw(hex).toCanonical().toLowerCase())
+    }
+
+    return
+  }
+
+  // HEX to ULID/UUID
+  if (Ulid.isRaw(value)) {
+    const ulid = Ulid.fromRaw(value).toCanonical()
+
+    if (mode !== 'uuid') {
+      console.log(ulid)
+    }
+
+    if (mode !== 'ulid') {
+      console.log(Uuid.fromRaw(value).toCanonical().toLowerCase())
+    }
+
+    return
+  }
+
+  // UUID to ULID/HEX
+  if (Uuid.isCanonical(value)) {
+    const hex = Uuid.fromCanonical(value).toRaw()
+
+    if (mode !== 'ulid') {
+      console.log(`0x${hex}`)
+    }
+
+    if (mode !== 'uuid') {
+      console.log(Ulid.fromRaw(hex).toCanonical())
+    }
+
+    return
+  }
+
+  console.error('Not sure what to do with this value')
+})();
